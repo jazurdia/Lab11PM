ba21c866037fb485c12332d42efa163eb1c3eb48
Javier Alejandro Azurdia Arrecis <78101285+jazurdia@users.noreply.github.com>
2022-10-13 07:36:35 -0600

    así se resolvía el problema

--- app/src/main/java/com/example/lab11pm/datasource/api/RickMortyApi.kt
+++ app/src/main/java/com/example/lab11pm/datasource/api/RickMortyApi.kt
@@ -4,6 +4,7 @@ import com.example.lab11pm.datasource.model.CharactersResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import com.example.lab11pm.datasource.model.Character
 
 interface RickMortyApi {
 
